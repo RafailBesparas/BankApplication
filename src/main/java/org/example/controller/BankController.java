b2cfd5616434b78a2116ed8fc65768961a8c91cf
RafaelBesparas <58226725+RafaelBesparas@users.noreply.github.com>
2025-04-13 13:29:24 +0200

    Finished Bank Controller / Still Many errors

--- src/main/java/org/example/controller/BankController.java
+++ src/main/java/org/example/controller/BankController.java
@@ -57,5 +57,46 @@ public class BankController {
         return "redirect:/dashboard";
     }
 
+    @PostMapping("/withdraw")
+    public String withdraw(@RequestParam BigDecimal amount, Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        AccountModel account = accountService.findAccountByUsername(username);
+
+        try {
+            accountService.withdraw(account, amount);
+        } catch (RuntimeException e){
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("account", account);
+            return "dashboard";
+        }
+
+        return "redirect:/dashboard";
+    }
+
+    @GetMapping("/transactions")
+    public String transactionHistory(Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        AccountModel account = accountService.findAccountByUsername(username);
+        model.addAttribute("transactions", accountService.getTransactionHistory(account));
+        return "transactions";
+    }
+
+    @PostMapping("/transfer")
+    public String transferAmount(@RequestParam String toUsername,
+                                 @RequestParam BigDecimal amount,
+                                 Model model){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        AccountModel fromAccount = accountService.findAccountByUsername(username);
+
+        try{
+            accountService.transferAmount(fromAccount, toUsername, amount);
+        }catch (RuntimeException e){
+            model.addAttribute("error", e.getMessage());
+            model.addAttribute("account", fromAccount);
+            return "dashboard";
+        }
+        return "redirect:/dashboard";
+    }
+
 
 }
